d82683e36f870f9edb017b74089b3ca6407edb00
macoding1994 <49666013+macoding1994@users.noreply.github.com>
2020-05-07 11:47:42 +0800

    5.1.2

---
+++ DBlogs/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for DBlogs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DBlogs.settings')
+profile = os.environ.get('TYPEIDEA_PROFILE', 'develop')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DBlogs.settings.%s" % profile)
+
+application = get_wsgi_application()
